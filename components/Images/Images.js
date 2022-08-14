d8aade31077e29fba4d62a6dafeec775f3393c21
JACK99-JACK99 <jitenmaurya0@gmail.com>
2022-08-14 18:21:58 +0530

    #1 added imags successfully to the card

--- components/Images/Images.js
+++ components/Images/Images.js
@@ -1,11 +1,11 @@
-import vmp from "../../public/images/vmp.png"
-import igame from "../../public/images/igame.png"
-import batzawaj from "../../public/images/batzawaj.png"
+// import vmp from "../../public/images/vmp.png"
+// import igame from "../../public/images/igame.png"
+// import batzawaj from "../../public/images/batzawaj.png"
 
-const images = {
-    vmp,
-    igame,
-    batzawaj
-}
+// const images = {
+//     vmp,
+//     igame,
+//     batzawaj
+// }
 
-export default images;
+// export default images;
